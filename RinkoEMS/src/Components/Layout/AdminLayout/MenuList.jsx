4276699007db2d8d8f94cc3297626bc44ac6ad81
Md Abuzar <105281118+abuzarmd-ML@users.noreply.github.com>
2024-04-14 18:19:58 +0200

    Added Collapsable side bar

--- RinkoEMS/src/Components/Layout/AdminLayout/MenuList.jsx
+++ RinkoEMS/src/Components/Layout/AdminLayout/MenuList.jsx
@@ -2,14 +2,51 @@ import * as React from 'react';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import ListSubheader from '@mui/material/ListSubheader';
-import DashboardIcon from '@mui/icons-material/Dashboard';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import PeopleIcon from '@mui/icons-material/People';
-import BarChartIcon from '@mui/icons-material/BarChart';
-import LayersIcon from '@mui/icons-material/Layers';
-import AssignmentIcon from '@mui/icons-material/Assignment';
+import DashboardIcon from '@mui/icons-material/Dashboard'
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart'
+import BarChartIcon from '@mui/icons-material/BarChart'
+import AssignmentIcon from '@mui/icons-material/Assignment'
+import List from '@mui/material/List';
+import Collapse from '@mui/material/Collapse';
 import { Link } from 'react-router-dom';
+import PeopleIcon from '@mui/icons-material/People';
+import AddIcon from '@mui/icons-material/Add';
+import ListIcon from '@mui/icons-material/List';
+
+const SubMenu = () => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <React.Fragment>
+      <ListItemButton onClick={handleClick}>
+        <ListItemIcon>
+          <PeopleIcon />
+        </ListItemIcon>
+        <ListItemText primary="Manage Employee" />
+      </ListItemButton>
+      <Collapse in={open} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding>
+          <ListItemButton component={Link} to="/employee/add">
+            <ListItemIcon>
+              <AddIcon />
+            </ListItemIcon>
+            <ListItemText primary="Add Employee" />
+          </ListItemButton>
+          <ListItemButton component={Link} to="/employee">
+            <ListItemIcon>
+              <ListIcon />
+            </ListItemIcon>
+            <ListItemText primary="Employee List" />
+          </ListItemButton>
+        </List>
+      </Collapse>
+    </React.Fragment>
+  );
+};
 
 export const mainListItems = (
   <React.Fragment>
@@ -27,16 +64,11 @@ export const mainListItems = (
     </ListItemButton>
     <ListItemButton component={Link} to="/obra">
       <ListItemIcon>
-        <LayersIcon />
+        <ShoppingCartIcon />
       </ListItemIcon>
       <ListItemText primary="Obra" />
     </ListItemButton>
-    <ListItemButton component={Link} to="/employee">
-      <ListItemIcon>
-        <PeopleIcon />
-      </ListItemIcon>
-      <ListItemText primary="Manage Employee" />
-    </ListItemButton>
+    <SubMenu />
     <ListItemButton component={Link} to="/attendance">
       <ListItemIcon>
         <BarChartIcon />
