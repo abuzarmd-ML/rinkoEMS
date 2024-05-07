2434853ec5a394e43d76c0c708199703d7b3dbdf
Bushra parween <bushraparween2017@gmail.com>
2024-05-07 22:45:39 +0530

    Changed Dropdown

--- RinkoEMS/src/Components/EmployeeForm/BasicDetails.jsx
+++ RinkoEMS/src/Components/EmployeeForm/BasicDetails.jsx
@@ -4,15 +4,21 @@ import TextareaAutosize from '@mui/material/TextareaAutosize';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import InputAdornment from '@mui/material/InputAdornment';
-import { Grid, Typography } from '@mui/material';
+import { Grid, Typography, Select, MenuItem, InputLabel,FormControl} from '@mui/material';
 import Cards from '../Cards/Cards';
 
+
 const BasicDetails = ({ fields }) => {
+
+  const [country, setCountry] = React.useState('');
+
+  const handleChange = (event) => {
+    setCountry(event.target.value);
+  };
   const handleSubmit = (event) => {
     event.preventDefault();
     // Implement form submission logic here
   };
-
   return (
     <Cards borderRadius={1} height={'400'}>
       <Grid container spacing={1} >
@@ -41,7 +47,7 @@ const BasicDetails = ({ fields }) => {
             variant="outlined"
           />
         </Grid>
-        <Grid item xs={6}>
+        {/* <Grid item xs={6}>
           <TextField
             required
             id={'test'}
@@ -49,7 +55,27 @@ const BasicDetails = ({ fields }) => {
             label={'Country'}
             variant="outlined"
           />
-        </Grid>
+        </Grid> */}
+      <Grid item xs={6}>
+      <FormControl variant="outlined" fullWidth>
+      <InputLabel id="country-label">Country</InputLabel>
+      <Select 
+        labelId="country-label"
+        id="country"
+        fullWidth
+        label={'Country'}
+        value={country}
+        onChange={handleChange}
+        variant="outlined"
+      >
+        <MenuItem value="">Select Country</MenuItem>
+        <MenuItem value="USA">United States</MenuItem>
+        <MenuItem value="UK">United Kingdom</MenuItem>
+        <MenuItem value="CA">Canada</MenuItem>
+        {/* Add more countries as needed */}
+      </Select>
+      </FormControl>
+    </Grid>
         <Grid item xs={6}>
           <TextField
             required
