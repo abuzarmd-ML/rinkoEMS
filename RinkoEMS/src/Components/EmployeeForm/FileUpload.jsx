6c235dfe8cb3ba5f4832c4926c2a0cae774c7351
Bushra parween <bushraparween2017@gmail.com>
2024-05-13 16:37:41 +0530

    reset file input- File upload

--- RinkoEMS/src/Components/EmployeeForm/FileUpload.jsx
+++ RinkoEMS/src/Components/EmployeeForm/FileUpload.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { TextField, Button, Grid, Typography, Card, CardContent } from '@mui/material';
 
 const DocumentUpload = ({ fields }) => {
@@ -7,6 +7,11 @@ const DocumentUpload = ({ fields }) => {
   const [NIE, setNIE] = useState(null);
   const [License, setLicense] = useState(null);
   const [contract, setContract] = useState(null);
+  const NIEInputRef = useRef(null);
+  const photoInputRef = useRef(null);
+  const ResumPInputRef = useRef(null);
+  const ContractInputRef = useRef(null);
+  const DLInputRef = useRef(null);
 
   const handlePhotoChange = (event) => {
     setPhoto(event.target.files[0]);
@@ -19,11 +24,33 @@ const DocumentUpload = ({ fields }) => {
     setNIE(event.target.files[0]);
   };
 
-  const handleDLDocumentChange = (event) => {
+  const handleDLChange = (event) => {
     setLicense(event.target.files[0]);
   };
   const handleContractChange = (event) => {
     setContract(event.target.files[0]);
+  };
+
+ 
+  const resetPhotoInput = () => {
+    setPhoto(null);
+    photoInputRef.current.value = null;
+  };
+
+  const resetNIEInput = () => {
+    NIEInputRef.current.value = null;
+  };
+
+  const resetDLInput = () => {
+    DLInputRef.current.value = null;
+  };
+
+  const resetResumeInput = () => {
+    ResumPInputRef.current.value = null;
+  };
+
+  const resetContractInput = () => {
+    ContractInputRef.current.value = null;
   };
 
   const handleSubmit = async (event) => {
@@ -64,44 +91,66 @@ const DocumentUpload = ({ fields }) => {
           </Grid>
 
           <Grid item xs={12} sx={{ display: 'flex', alignItems: 'center' }}>
-          <Typography variant="body1" sx={{ marginRight: '120px',fontSize: '15px', fontWeight: 'bold' }}>Upload Photo (JPEG/PNG):</Typography>
-           <input
+            <Typography variant="body1" sx={{ marginRight: '120px', fontSize: '15px', fontWeight: 'bold' }}>Upload Photo (JPEG/PNG):</Typography>
+            <input
+              ref={photoInputRef}
               type="file"
               accept="image/*"
               onChange={handlePhotoChange}
             />
+            {photo && (
+              <Button onClick={resetPhotoInput}>X</Button>
+            )}
           </Grid>
+
           <Grid item xs={12} sx={{ display: 'flex', alignItems: 'center' }}>
-          <Typography variant="body1" sx={{ marginRight: '140px',fontSize: '15px', fontWeight: 'bold' }}>Upload NIE (JPEG/PNG):</Typography>
-           <input
+            <Typography variant="body1" sx={{ marginRight: '140px', fontSize: '15px', fontWeight: 'bold' }}>Upload NIE (JPEG/PNG):</Typography>
+            <input
+              ref={NIEInputRef}
               type="file"
               accept="image/*"
               onChange={handleNIEChange}
             />
+            {NIE && (
+              <Button onClick={resetNIEInput}>X</Button>
+            )}
           </Grid>
           <Grid item xs={12} sx={{ display: 'flex', alignItems: 'center' }}>
-          <Typography variant="body1" sx={{ marginRight: '50px',fontSize: '15px', fontWeight: 'bold' }}>Upload Driving License (JPEG/PNG):</Typography>
-           <input
+
+            <Typography variant="body1" sx={{ marginRight: '49px', fontSize: '15px', fontWeight: 'bold' }}>Upload Driving License (JPEG/PNG):</Typography>
+            <input
+              ref={DLInputRef}
               type="file"
               accept="image/*"
-              onChange={handleDLDocumentChange}
+              onChange={handleDLChange}
             />
+            {License && (
+              <Button onClick={resetDLInput}>X</Button>
+            )}
           </Grid>
           <Grid item xs={12} sx={{ display: 'flex', alignItems: 'center' }}>
-          <Typography variant="body1" sx={{ marginRight: '150px',fontSize: '15px', fontWeight: 'bold' }}>Upload Resume (PDF):</Typography>
+            <Typography variant="body1" sx={{ marginRight: '150px', fontSize: '15px', fontWeight: 'bold' }}>Upload Resume (PDF):</Typography>
             <input
+              ref={ResumPInputRef}
               type="file"
               accept=".pdf"
               onChange={handleResumeDocumentChange}
             />
+            {resumeDocument && (
+              <Button onClick={resetResumeInput}>X</Button>
+            )}
           </Grid>
           <Grid item xs={12} sx={{ display: 'flex', alignItems: 'center' }}>
-          <Typography variant="body1" sx={{ marginRight: '150px',fontSize: '15px', fontWeight: 'bold' }}>Upload Contract (PDF):</Typography>
+            <Typography variant="body1" sx={{ marginRight: '150px', fontSize: '15px', fontWeight: 'bold' }}>Upload Contract (PDF):</Typography>
             <input
+              ref={ContractInputRef}
               type="file"
               accept=".pdf"
               onChange={handleContractChange}
             />
+            {contract && (
+              <Button onClick={resetContractInput}>X</Button>
+            )}
           </Grid>
           <Grid item xs={12}>
             <Button variant="contained" color="primary" onClick={handleSubmit}>
