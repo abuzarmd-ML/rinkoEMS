c57134170bb8568233471fbcad8d700e9295ab93
Md Abuzar <105281118+abuzarmd-ML@users.noreply.github.com>
2024-05-18 02:30:35 +0200

    added new generic component, component for company frontend

--- RinkoEMS/src/Components/Company/AddCompany.jsx
+++ RinkoEMS/src/Components/Company/AddCompany.jsx
@@ -1,15 +1,17 @@
 import React,{useState,useEffect} from 'react';
 import { useForm,Form } from '../BasicForm/useForm';
 import { Grid, TextField } from '@mui/material';
+import FormInputElement from '../BasicForm/FormInputElement';
+import Controls from '../BasicForm/Controls';
+import { getStatus } from '../../services/statusService';
 
 const initialFValues = {
     companyName: '',
     address:'',
     nif: 0,
     status:'',
-    encarger:'',
     contactNumber:0,
-    startDate: new Date()
+    // startDate: new Date(),
 }
 // const mandatoryError = 'This field is mandatory'
 
@@ -17,21 +19,52 @@ const initialFValues = {
 export default function AddCompany() {
 
     const {values,setValues,handleInputChange} = useForm(initialFValues);
+    const statusOptions = getStatus();
 
     return (
         <Form >
             <Grid container>
                 <Grid item xs={6}>
-                <TextField
-                    variant="outlined"
-                    label="Company Name"
-                    name="companyName"
-                    value={values.companyName}
-                    onChange={handleInputChange}
-                />
-
-
+                    <FormInputElement
+                        name="companyName"
+                        label="Company Name"
+                        value={values.companyName}
+                        onChange={handleInputChange}
+                    />
+                    <FormInputElement
+                        name="address"
+                        label="Address"
+                        value={values.address}
+                        onChange={handleInputChange}
+                    />
+                    <FormInputElement
+                        name="contactNumber"
+                        label="Contact Number"
+                        value={values.contactNumber}
+                        onChange={handleInputChange}
+                    />
                 </Grid>
+                
+                <Grid item xs={6}>
+                    <FormInputElement
+                        name="nif"
+                        label="NIF"
+                        value={values.nif}
+                        onChange={handleInputChange}
+                    />
+                    <Controls.Select
+                    name = "status"
+                    label = "Status"
+                    value = {values.status}
+                    onChange = {handleInputChange}
+                    options = {statusOptions}
+                    />
+                </Grid>
+            <div>
+                <Controls.Button
+                    text = "Submit"
+                />
+            </div>
             </Grid>
         </Form>
     )
