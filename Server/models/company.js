2ca39a1747c2e85c1128854a7443773678190008
Md Abuzar <105281118+abuzarmd-ML@users.noreply.github.com>
2024-05-25 00:25:59 +0200

    developed Frontend and backend for company

--- Server/models/company.js
+++ Server/models/company.js
@@ -4,7 +4,20 @@ import config from '../config/database.js';
 
 const pool = mysql.createPool(config);
 
-export async function getAllCompanies() {
+async function createCompany(name,address,encargar,status) {
+  const connection = await pool.getConnection();
+  try {
+    const [result] = await connection.execute(
+      'INSERT INTO companies (name,address,encargar,status) VALUES (?, ?, ?, ?)',
+      [name,address,encargar,status]
+    );
+    return result.insertId;
+  } finally {
+    connection.release();
+  }
+}
+
+async function getCompanyName() {
   const connection = await pool.getConnection();
   try {
     const [rows] = await connection.execute('SELECT company_id, name FROM companies');
@@ -13,3 +26,5 @@ export async function getAllCompanies() {
     connection.release();
   }
 }
+
+export {createCompany,getCompanyName}
